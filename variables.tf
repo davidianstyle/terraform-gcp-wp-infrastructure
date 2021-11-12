41e945e027c175a2797fb594eb70bb5d002992f2
David Chang <dchang@davidianstyle.com>
2021-11-12 11:57:20 -0500

    Update GCP Project ID + GCP regions

--- variables.tf
+++ variables.tf
@@ -3,19 +3,19 @@
 //Project ID Variable
 variable "project_id" {
   type = string
-  default = "---YOUR_PROJECT_ID---"
+  default = "sandbox-20210730-z4re8f"
 }
 
 //Region1 Variable
 variable "region1" {
   type = string
   description = "Region1 Name"
-  default = "us-west1"
+  default = "us-central1"
 }
 
 //Region2 Variable
 variable "region2" {
   type = string
   description = "Region2 Name"
-  default = "us-central1"
+  default = "us-east1"
 }
